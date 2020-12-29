dca0b7f066aa0596486e496c7a4eacf1331ed8b6
Mavrogiannis, Christos <Christos.Mavrogiannis@ottobock.de>
2020-12-29 12:14:18 +0100

    End of section 4 - Add Logic to your application

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, jsonify
 from model import db
 
 # Flask constructor
@@ -12,12 +12,8 @@ app = Flask(__name__)
 def welcome():
     return render_template(
         "welcome.html", 
-        message = 'Here is a meesage from the view.')
-
-@app.route("/card")
-def card_view():
-    card = db[0]
-    return render_template("card.html", card=card)
+        cards = db
+    )
 
 counter = 0
 @app.route("/count_views")
@@ -25,3 +21,27 @@ def count_views():
     global counter #We use global, because the variable is declared outside the local scope 
     counter += 1
     return "This page was servers " + str(counter) + " times." 
+
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html",
+                                card=card, 
+                                index=index,
+                                max_index=len(db) - 1
+        )
+    except IndexError:
+        abort(404)
+
+@app.route("/api/card/")
+def api_card_list():
+    return jsonify(db)
+
+
+@app.route('/api/card/<int:index>')
+def api_card_detail(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
