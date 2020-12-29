c7b693c6799213504b10e92362919d29231296b3
Mavrogiannis, Christos <Christos.Mavrogiannis@ottobock.de>
2020-12-29 13:32:26 +0100

    End of section 5 - Add user interaction

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, abort, jsonify
-from model import db
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
+from model import db, save_db
 
 # Flask constructor
 # Creates a global Flask application object
@@ -33,6 +33,36 @@ def card_view(index):
         )
     except IndexError:
         abort(404)
+
+@app.route('/add_card', methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        # form has been submitted, process data
+        card = {
+            "question": request.form['question'],
+            "answer": request.form['answer']
+        }
+
+        db.append(card)
+        save_db()
+        return redirect(url_for('card_view', index=len(db)-1))
+    else:
+        return render_template("add_card.html")
+
+
+@app.route('/remove_card/<int:index>', methods=["GET", "POST"])
+def remove_card(index):
+    try:
+        if request.method == "POST":
+            del db[index]
+            save_db()
+            return redirect(url_for('welcome'))
+        else:
+            return render_template("remove_card.html", card=db[index])
+    except IndexError:
+        abort(404)
+
+## ----- API Methods -----
 
 @app.route("/api/card/")
 def api_card_list():
