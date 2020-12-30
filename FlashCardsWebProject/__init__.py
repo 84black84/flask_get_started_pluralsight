76863156aca39946528997fdec94448151ac6747
Mavrogiannis, Christos <Christos.Mavrogiannis@ottobock.de>
2020-12-30 07:26:52 +0100

    Remove DebugToolbarExtension

--- FlashCardsWebProject/__init__.py
+++ FlashCardsWebProject/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
 # Flask constructor
 # Creates a global Flask application object
@@ -7,8 +7,8 @@ from flask_debugtoolbar import DebugToolbarExtension
 # in our case is the name of the current python file, which is 'flashcards'.
 app = Flask(__name__)
 # app.debug = True
-app.config['SECRET_KEY'] = '3945ec69-1750-4912-94da-378366941c50'
-toolbar = DebugToolbarExtension(app)
+# app.config['SECRET_KEY'] = '3945ec69-1750-4912-94da-378366941c50'
+# toolbar = DebugToolbarExtension(app)
 
 import FlashCardsWebProject.views
 import FlashCardsWebProject.cardApi
